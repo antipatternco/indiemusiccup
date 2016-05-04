f0b61956f720ea48e1627bb3e2df536230f30431
Paul Carvill <paul@paulcarvill.com>
2016-05-04 21:22:32 +0100

    Update 2016-05-04T20:22:30.911Z

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./**/*')
+    .pipe(ghPages());
+});
