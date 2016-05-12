10e8c5903695ded62e9fea6e035d31ff74fb1aca
Paul Carvill <paulcarvill@google.com>
2016-05-12 13:18:07 +0100

    Update 2016-05-12T12:18:05.603Z

--- js.js
+++ js.js
@@ -157,7 +157,7 @@
             x: renderer.width > 600 ? renderer.width * 0.284 : renderer.width * 0.8,
             y: renderer.width > 600 ? renderer.height * 0.27 : renderer.height * 0.3,
             styles: {
-                src: 'assets/FABRIC.svg'
+                src: 'assets/TRANGRESSIVE.svg'
             }
         });
 
