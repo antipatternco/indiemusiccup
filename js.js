748f95e7d3d212b3e595ff9aaf4130404a7ffaa8
Paul Carvill <paulcarvill@google.com>
2016-05-12 13:19:09 +0100

    Update 2016-05-12T12:19:08.129Z

--- js.js
+++ js.js
@@ -145,7 +145,6 @@
 
         });
 
-
         var player1 = Physics.body('player', {
             x: renderer.width > 600 ? renderer.width * 0.284 : renderer.width * 0.8,
             y: renderer.width > 600 ? renderer.height * 0.27 : renderer.height * 0.3,
